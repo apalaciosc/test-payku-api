5bdacec1f8cedf512dc07ea7a8a060040c77a211
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-08-12 15:47:16 -0500

    ADD: Transaction methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'notify_payku', to: 'transactions#notify_payku', as: :notify_payku
 end
