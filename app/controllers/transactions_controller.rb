08aa09dbc1dce914965122968c1eba6b69f4f0f1
Alvaro Palacios Carrillo <apalacioscarrillo@gmail.com>
2019-08-12 16:45:42 -0500

    ADD: Save token transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,14 +1,19 @@
 class TransactionsController < ApplicationController
 
   def notify_payku
-    order_id         = params['order_id']
-    transaction_id   = params['transaction_id'] #Payer_ID
-    status           = params['status'].to_i #Success: 1
-    verification_key = params['verification_key']
-    transaction      = Transaction.find_by_transaction_id(order_id.to_i)
+    order_id              = params['order_id']
+    transaction_id        = params['transaction_id'] #Payer_ID
+    status                = params['status'].to_i #Success: 1
+    verification_key      = params['verification_key']
+    transaction           = Transaction.find_by_transaction_id(order_id.to_i)
     return unless transaction.present?
     return unless status == 1
-    success_pay      = transaction.verify_payment_payku(transaction_id, verification_key)
+    success_pay           = transaction.verify_payment_payku(transaction_id, verification_key)
+    transaction.payerid   = transaction_id
+    transaction.step      = :completed
+    transaction.respuesta = 'Pago con PayKu correcto.'
+    transaction.token     = verification_key
+    transaction.save
     render json: success_pay, status: 200
   end
 
